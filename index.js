51d88d3724ae2a4aaa7e5b6f1ee447ee9aac6be0
cpuntier <cpuntier@gmail.com>
2023-12-03 11:18:53 -0500

    separated things into functions

--- index.js
+++ index.js
@@ -76,6 +76,33 @@ const LearnerSubmissions = [
     }
 ];
 
+function validateID(course, ag) {
+    try {
+        if (ag.course_id !== course.id) {
+            throw "The assignments in this group do not belong to the inputted course. Please make sure they are related.";
+        } else {
+            console.log("Starting calculations....")
+        }
+    } catch (error) {
+        console.log(error);
+        return false;
+    }
+    return true;
+}
+
+function obtainLearnerID(submissions){
+    output = []
+    for (i in submissions) {
+        if (!(output.indexOf(submissions[i].learner_id) !== -1)) {
+
+            output.push(submissions[i].learner_id);
+        }
+    }
+    return output;
+
+}
+
+
 function getLearnerData(course, ag, submissions) {
     // here, we would process this data to achieve the desired result.
     // console.log("ag\n", ag);
@@ -83,32 +110,21 @@ function getLearnerData(course, ag, submissions) {
     // console.log("submissions\n",submissions);
 
     //first check if course id matches ag course_id
-    try {
-        if(ag.course_id !== course.id){
-            throw  "The assignments in this group do not belong to the inputted course. Please make sure they are related.";
-        }else{
-            console.log("Starting calculations....")
-        }
-    } catch (error) {
-        console.log(error);
+
+    if(!validateID(course, ag)){
         return;
-    }
-
-    console.log("assignment group course id:",ag.course_id);
-    console.log("course id",course.id);
-
-
+    };
     let myResult = [];
-    let learnerIDArray = [];
-    for(i in submissions){
-        if (!( learnerIDArray.indexOf(submissions[i].learner_id) !== -1)){
 
-        learnerIDArray.push(submissions[i].learner_id);
-        }
-    }
+    console.log("assignment group course id:", ag.course_id);
+    console.log("course id", course.id);
+
+
+    let learnerIDArray = obtainLearnerID(submissions);
     console.log(learnerIDArray);
     //found ID's belonging to learners that provided submissions
 
+    
 
     ////USE THIS LATER ITS A SURPRISE TOOL
     // console.log(submissions.filter((submission) => {
@@ -138,4 +154,4 @@ function getLearnerData(course, ag, submissions) {
 // const result = getLearnerData(CourseInfo, AssignmentGroup, LearnerSubmissions);
 // 
 // console.log(result);
-getLearnerData(CourseInfo,AssignmentGroup,LearnerSubmissions);
+getLearnerData(CourseInfo, AssignmentGroup, LearnerSubmissions);
